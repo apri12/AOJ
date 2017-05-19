eae163203265be722c11efae95e231c188bfcd65
apri12 <apri12@apri12.com>
2017-05-20 00:08:07 +0900

    fix bug 0004

--- cpp/Volume0/Simultaneous_Equation_0004.cpp
+++ cpp/Volume0/Simultaneous_Equation_0004.cpp
@@ -5,9 +5,9 @@ int main(){
   double a,b,c,d,e,f;
   while(cin >> a >> b >> c >> d >> e >>f){
     double delta = a*e - d*b;
-    double x = (1/delta)*(e*c+(-b)*f);
-    double y = (1/delta)*((-d)*c+a*f);
+    double x = (1/delta)*(e*c+(-b)*f)+0;
+    double y = (1/delta)*((-d)*c+a*f)+0;
     //cout << x << " " <<y << endl;
-    printf("%4.3f 4.3%f\n",x,y);
+    printf("%4.3f %4.3f\n",x,y);
   }
 }
